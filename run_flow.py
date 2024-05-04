6d81c95592e6cd55413b9a69e2ba42c4cba886bc
scbtm <san.cbtm@gmail.com>
2024-05-04 02:13:55 -0600

    fix bucket reading issue in metaflow

--- run_flow.py
+++ run_flow.py
@@ -1,7 +1,13 @@
 import os
 import sys
+import polars as pl
 
 def main():
+    #read data file and save it locally
+    bucket_path = os.getenv('BUCKET_PATH')
+    temporary_data_file = os.getenv('INPUT_DATA_PATH')
+    df = pl.read_csv(bucket_path)
+    df.to_csv(temporary_data_file)
     flow = os.getenv('META_FLOW', 'default_flow.py')  # Get the flow name from an environment variable
     os.system(f"python {flow} run")
 
