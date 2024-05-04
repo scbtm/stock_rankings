1c6f380dd17269a2aabc7160000c396804d258f3
scbtm <san.cbtm@gmail.com>
2024-05-04 02:23:49 -0600

    fix bucket reading issue with polars

--- run_flow.py
+++ run_flow.py
@@ -1,13 +1,12 @@
 import os
-import sys
-import polars as pl
+import pandas as pd
 
 def main():
     #read data file and save it locally
     bucket_path = os.getenv('BUCKET_PATH')
     temporary_data_file = os.getenv('INPUT_DATA_PATH')
-    df = pl.read_csv(bucket_path)
-    df.to_csv(temporary_data_file)
+    df = pd.read_csv(bucket_path)
+    df.to_csv(temporary_data_file, index = False)
     flow = os.getenv('META_FLOW', 'default_flow.py')  # Get the flow name from an environment variable
     os.system(f"python {flow} run")
 
