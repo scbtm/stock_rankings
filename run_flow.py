bd9481adccd78cbb985457572050db64fe9fbe82
scbtm <san.cbtm@gmail.com>
2024-05-04 02:32:32 -0600

    added missing dependencies to read from gcs

--- run_flow.py
+++ run_flow.py
@@ -3,10 +3,10 @@ import pandas as pd
 
 def main():
     #read data file and save it locally
-    bucket_path = os.getenv('BUCKET_PATH')
-    temporary_data_file = os.getenv('INPUT_DATA_PATH')
-    df = pd.read_csv(bucket_path)
-    df.to_csv(temporary_data_file, index = False)
+    #bucket_path = os.getenv('BUCKET_PATH')
+    #temporary_data_file = os.getenv('INPUT_DATA_PATH')
+    #df = pd.read_csv(bucket_path)
+    #df.to_csv(temporary_data_file, index = False)
     flow = os.getenv('META_FLOW', 'default_flow.py')  # Get the flow name from an environment variable
     os.system(f"python {flow} run")
 
